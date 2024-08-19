5c82c51d4eef5908d319eaa76e58cb76957685d2
EdwardMelendezM <edward.melendez.mendigure@gmail.com>
2024-08-18 20:59:08 -0500

    add changes

--- web-scraping/domain/web_scraping_entity.go
+++ web-scraping/domain/web_scraping_entity.go
@@ -1,2 +1,7 @@
 package domain
 
+type SearchResult struct {
+	Title   string `json:"title"`
+	URL     string `json:"url"`
+	Content string `json:"content"`
+}
--- web-scraping/domain/web_scraping_repository.go
+++ web-scraping/domain/web_scraping_repository.go
@@ -1,2 +1,7 @@
 package domain
 
+type WebScrapingRepository interface {
+	VerifyExistsUrl(url string) (bool, error)
+	GetLastNumber() (*int, error)
+	CreateRecord()
+}
